4e62077374222c20c15dba34905beb5171efcbc3
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2024-10-02 02:54:56 +0200

    Added a section for user's feedback

--- birthday-form/src/App.jsx
+++ birthday-form/src/App.jsx
@@ -1,9 +1,26 @@
-
+import { useState } from "react";
 import "./style.css"
 function App() {
+  const[name, setName]= useState("");
+  const[score, setScore]= useState("10");
+   const[comment, setComment]= useState(";")
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if (Number(score) <= 5 && comment.length <= 10){
+      alert("Please provide a comment explaining why the exprience was poor");
+      return;
+    }
+    console.log("Form submitted!");
+    setName("");
+    setScore("10");
+    setComment("");
+ 
+  }
 
 
   return (
+    <>
     <form>
     <label htmlFor="res-date">Choose date</label>
     <input type="date" id="res-date"/>
@@ -19,7 +36,7 @@ function App() {
     </select>
 
     <label htmlFor="guests">Number of guests</label>
-    <input type="number" placeholder="1" min="1" max="10" id="guest" />
+    <input type="number" placeholder="1" min="10" max="500" id="guest" />
     <label htmlFor="occasion">Occasion</label>
     <select id="occasion">
       <option >Birthday</option>
@@ -27,6 +44,44 @@ function App() {
     </select>
     <input type="Submit" value="Make Your reservation"/>
     </form>
+
+
+  
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+     <div className="Field">
+      <label htmlFor="name">Name</label>
+      <input
+      id="name"
+      type="text"
+       placeholder="Name" 
+       name="name" 
+       value={name} onChange={e => setName(e.target.value)} />
+     </div>
+
+     <div className="field">
+      <label htmlFor="range">Score:{score}</label>
+      <input type="range" 
+      min="0"
+       max="10" 
+       value={score}
+        onChange={e => setScore(e.target.value)} />
+     </div>
+
+     <div className="Field">
+ <label>
+comment:
+<textarea value={comment} onChange = {e => setComment(e.target.value)}/>
+ </label>
+     </div>
+
+     <button disabled={!name} type="submit">Submit</button>
+        </fieldset>
+
+      </form>
+    </div>
+    </>
   )
 }
 
